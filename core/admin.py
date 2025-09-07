c9d34522719432de00c921c049ef966179ecbbad
papi721-main <99392252+papi721-main@users.noreply.github.com>
2025-09-07 15:17:35 +0300

    feat: add a User registration form for the Admin

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "usable_password",
+                    "password1",
+                    "password2",
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "is_staff",
+                    "date_of_birth",
+                    "phone_number",
+                ),
+            },
+        ),
+    )
